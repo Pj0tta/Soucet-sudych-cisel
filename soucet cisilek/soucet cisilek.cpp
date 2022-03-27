d98a02148bc0f14235b33badd2fee3b4c8cc5faa
Petr Habel <petrhabel04@gmail.com>
2022-03-27 16:30:10 +0200

    Přidejte soubory projektu.

---
+++ soucet cisilek/soucet cisilek.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+	int hodnota;
+	int sude = 0;
+	printf("Napis hodnotu cisla: ");
+	scanf_s("%d", &hodnota);
+
+	for (int i = 1; i <= hodnota; i++) {
+		if (i % 2 == 0) {
+			sude = sude + i;
+		}
+	}
+	printf("\nPocet sudych cisel: %d:", sude);
+
+}
+
